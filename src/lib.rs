518b5b7c6c3093940dfa79f45f5faaf31b5f6727
Virjilakrum <asesenep15@gmail.com>
2024-07-29 17:37:51 +0300

    Add the following code to your src/lib.rs file to generate the key pair

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use solana_sdk::{signature::{Keypair, Signer}, pubkey::Pubkey};
+use std::fs::File;
+use std::io::Write;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn keygen() {
+        let kp = Keypair::new();
+        println!("You've generated a new Solana wallet: {}", kp.pubkey().to_string());
+        let keypair_bytes = kp.to_bytes();
+        let json = serde_json::to_string(&keypair_bytes).unwrap();
+        let mut file = File::create("dev-wallet.json").unwrap();
+        file.write_all(json.as_bytes()).unwrap();
     }
 }

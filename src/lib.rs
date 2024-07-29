d722a86876b8adf95fbda3c38b278e0d0807d60e
Virjilakrum <asesenep15@gmail.com>
2024-07-29 18:01:11 +0300

    update key pair generation as follows

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use base64::encode;
+use base64::{decode, encode};
+use serde::{Deserialize, Serialize};
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::signature::read_keypair_file;
 use solana_sdk::{
@@ -6,15 +7,23 @@ use solana_sdk::{
     signature::{Keypair, Signer},
 };
 use std::fs::File;
+use std::io::Read;
 use std::io::Write;
 
-const RPC_URL: &str = "https://api.devnet.solana.com";
+mod airdrop;
+
+#[derive(Serialize, Deserialize)]
+struct KeypairJson {
+    keypair: Vec<u8>,
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use solana_sdk::{system_instruction::transfer, transaction::Transaction};
     use std::str::FromStr;
+
+    const RPC_URL: &str = "https://api.devnet.solana.com";
 
     #[test]
     fn keygen() {
@@ -24,14 +33,22 @@ mod tests {
             kp.pubkey().to_string()
         );
         let keypair_bytes = kp.to_bytes();
-        let json = encode(&keypair_bytes);
+        let keypair_json = KeypairJson {
+            keypair: keypair_bytes.to_vec(),
+        };
+        let json = serde_json::to_string(&keypair_json).unwrap();
         let mut file = File::create("dev-wallet.json").unwrap();
         file.write_all(json.as_bytes()).unwrap();
     }
 
     #[test]
     fn airdrop() {
-        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+        let mut file = File::open("dev-wallet.json").expect("Couldn't find wallet file");
+        let mut json_str = String::new();
+        file.read_to_string(&mut json_str).unwrap();
+        let keypair_json: KeypairJson = serde_json::from_str(&json_str).unwrap();
+        let keypair = Keypair::from_bytes(&keypair_json.keypair).unwrap();
+
         let client = RpcClient::new(RPC_URL);
         match client.request_airdrop(&keypair.pubkey(), 2_000_000_000u64) {
             Ok(s) => {
@@ -47,7 +64,12 @@ mod tests {
 
     #[test]
     fn transfer_sol() {
-        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+        let mut file = File::open("dev-wallet.json").expect("Couldn't find wallet file");
+        let mut json_str = String::new();
+        file.read_to_string(&mut json_str).unwrap();
+        let keypair_json: KeypairJson = serde_json::from_str(&json_str).unwrap();
+        let keypair = Keypair::from_bytes(&keypair_json.keypair).unwrap();
+
         let to_pubkey = Pubkey::from_str("<your WBA public key>").unwrap();
         let client = RpcClient::new(RPC_URL);
         let recent_blockhash = client

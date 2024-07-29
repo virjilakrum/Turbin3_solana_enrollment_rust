c924606a1c6a920911298e3171d12c73d0783dff
Virjilakrum <asesenep15@gmail.com>
2024-07-29 18:19:20 +0300

    update with pub key

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,28 @@
+use serde::{Deserialize, Serialize}; // Serde modüllerini ekleyin
+use solana_client::rpc_client::RpcClient;
+use solana_sdk::signature::read_keypair_file;
+use solana_sdk::{
+    pubkey::Pubkey,
+    signature::{Keypair, Signer},
+};
+use std::fs::File;
+use std::io::{Read, Write};
+
 mod programs;
+
+#[derive(Serialize, Deserialize, Debug)] // Debug trait'i ekleyin
+struct KeypairJson {
+    keypair: Vec<u8>,
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::programs::wba_prereq::{CompleteArgs, UpdateArgs, WbaPrereqProgram};
-    use solana_client::rpc_client::RpcClient;
-    use solana_program::system_instruction::transfer;
-    use solana_sdk::{
-        bs58,
-        message::Message,
-        pubkey::Pubkey,
-        signature::{self, read_keypair_file, Keypair, Signer},
-        signer::keypair,
-        system_program,
-        transaction::{self, Transaction},
-    };
-    use std::io::{self, BufRead};
-    use std::str::FromStr;
+    use super::*;
+    use crate::programs::wba_prereq::{CompleteArgs, WbaPrereqProgram}; // Eksik importlar
+    use solana_sdk::message::Message; // Eksik import
+    use solana_sdk::system_program;
+    use solana_sdk::{system_instruction::transfer, transaction::Transaction};
+    use std::str::FromStr; // Eksik import
 
     const RPC_URL: &str = "https://api.devnet.solana.com";
 
@@ -26,76 +33,62 @@ mod tests {
         println!("You've generated a new Solana wallet: {}", pubkey);
         println!("");
         println!("To save your wallet, copy and paste the following into a JSON file:");
-        println!("{:?}", kp.to_bytes());
-    }
 
-    #[test]
-    fn base58_to_wallet() {
-        println!("Input your private key as base58:");
-        let stdin = io::stdin();
-        let base58 = stdin.lock().lines().next().unwrap().unwrap();
-        println!("Your wallet file is:");
-        let wallet = bs58::decode(base58).into_vec().unwrap();
-        println!("{:?}", wallet);
-    }
+        let keypair_bytes = kp.to_bytes();
+        let keypair_json = KeypairJson {
+            keypair: keypair_bytes.to_vec(),
+        };
+        let json = serde_json::to_string(&keypair_json).unwrap();
+        let mut file = File::create("dev-wallet.json").unwrap();
+        file.write_all(json.as_bytes()).unwrap();
 
-    #[test]
-    fn wallet_to_base58() {
-        println!("Input your private key as a wallet file byte array:");
-        let stdin = io::stdin();
-        let wallet = stdin
-            .lock()
-            .lines()
-            .next()
-            .unwrap()
-            .unwrap()
-            .trim_start_matches('[')
-            .trim_end_matches(']')
-            .split(',')
-            .map(|s| s.trim().parse::<u8>().unwrap())
-            .collect::<Vec<u8>>();
-        println!("Your private key is:");
-        let base58 = bs58::encode(wallet).into_string();
-        println!("{:?}", base58);
+        println!("{:?}", keypair_json);
     }
 
     #[test]
     fn airdrop() {
-        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+        // Dosyadan keypair'i okuyun
+        let mut file = File::open("dev-wallet.json").expect("Couldn't find wallet file");
+        let mut json_str = String::new();
+        file.read_to_string(&mut json_str).unwrap();
+        let keypair_json: KeypairJson = serde_json::from_str(&json_str).unwrap();
+        let keypair = Keypair::from_bytes(&keypair_json.keypair).unwrap();
+
         let client = RpcClient::new(RPC_URL);
         match client.request_airdrop(&keypair.pubkey(), 2_000_000_000u64) {
             Ok(s) => {
-                println!("Success! Check out your tx here:");
+                println!("Success! Check out your TX here:");
                 println!(
                     "https://explorer.solana.com/tx/{}?cluster=devnet",
                     s.to_string()
                 );
             }
             Err(e) => println!("Oops, something went wrong: {}", e.to_string()),
-        }
+        };
     }
 
     #[test]
     fn transfer_sol() {
-        let keypair = read_keypair_file("dev-wallet.json").expect("Counldn't find wallet");
-        let to_pubkey = Pubkey::from_str("HWkiywmVgVmVzg3JtHivLrTrQrrKjMaP7mR8QKk84F7b").unwrap();
+        let mut file = File::open("dev-wallet.json").expect("Couldn't find wallet file");
+        let mut json_str = String::new();
+        file.read_to_string(&mut json_str).unwrap();
+        let keypair_json: KeypairJson = serde_json::from_str(&json_str).unwrap();
+        let keypair = Keypair::from_bytes(&keypair_json.keypair).unwrap();
 
-        let rpc_client = RpcClient::new(RPC_URL);
-        let recent_blockhash = rpc_client
+        let to_pubkey = Pubkey::from_str("HWkiywmVgVmVzg3JtHivLrTrQrrKjMaP7mR8QKk84F7b").unwrap();
+        let client = RpcClient::new(RPC_URL);
+        let recent_blockhash = client
             .get_latest_blockhash()
             .expect("Failed to get recent blockhash");
-
         let transaction = Transaction::new_signed_with_payer(
             &[transfer(&keypair.pubkey(), &to_pubkey, 1_000_000)],
             Some(&keypair.pubkey()),
             &vec![&keypair],
             recent_blockhash,
         );
-
-        let signature = rpc_client
+        let signature = client
             .send_and_confirm_transaction(&transaction)
             .expect("Failed to send transaction");
-
         println!(
             "Success! Check out your TX here: https://explorer.solana.com/tx/{}/?cluster=devnet",
             signature
@@ -104,15 +97,19 @@ mod tests {
 
     #[test]
     fn transfer_sol_balance() {
-        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet");
-        let to_pubkey = Pubkey::from_str("CcVWtVfQ3944MuNXbEXrkacqwtuy5uccZ5XydUKgyiE5").unwrap();
+        let mut file = File::open("dev-wallet.json").expect("Couldn't find wallet file");
+        let mut json_str = String::new();
+        file.read_to_string(&mut json_str).unwrap();
+        let keypair_json: KeypairJson = serde_json::from_str(&json_str).unwrap();
+        let keypair = Keypair::from_bytes(&keypair_json.keypair).unwrap();
 
-        let rpc_client = RpcClient::new(RPC_URL);
-        let recent_blockhash = rpc_client
+        let to_pubkey = Pubkey::from_str("CcVWtVfQ3944MuNXbEXrkacqwtuy5uccZ5XydUKgyiE5").unwrap();
+        let client = RpcClient::new(RPC_URL);
+        let recent_blockhash = client
             .get_latest_blockhash()
             .expect("Failed to get recent blockhash");
 
-        let balance = rpc_client
+        let balance = client
             .get_balance(&keypair.pubkey())
             .expect("Failed to get balance");
         let message = Message::new_with_blockhash(
@@ -121,7 +118,7 @@ mod tests {
             &recent_blockhash,
         );
 
-        let fee = rpc_client
+        let fee = client
             .get_fee_for_message(&message)
             .expect("Failed to get fee calculator");
 
@@ -132,7 +129,7 @@ mod tests {
             recent_blockhash,
         );
 
-        let signature = rpc_client
+        let signature = client
             .send_and_confirm_transaction(&transaction)
             .expect("Failed to send transaction");
         println!(
@@ -143,8 +140,7 @@ mod tests {
 
     #[test]
     fn consume_idl() {
-        let rpc_client = RpcClient::new(RPC_URL);
-
+        let client = RpcClient::new(RPC_URL);
         let signer = read_keypair_file("wba-wallet.json").expect("Couldn't find wallet");
 
         let prereq = WbaPrereqProgram::derive_program_address(&[
@@ -155,7 +151,7 @@ mod tests {
             github: b"0xBitzz".to_vec(),
         };
 
-        let blockhash = rpc_client
+        let blockhash = client
             .get_latest_blockhash()
             .expect("Failed to get recent blockhash");
 
@@ -167,7 +163,7 @@ mod tests {
             blockhash,
         );
 
-        let signature = rpc_client
+        let signature = client
             .send_and_confirm_transaction(&transaction)
             .expect("Failed to send transaction");
         println!(
